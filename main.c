417f9910fbea635d7acbb43633a7da69318f19d8
pavelkozh <72147163+pavelkozh@users.noreply.github.com>
2020-11-01 02:33:41 +0300

    RGB_sens.c

--- main.c
+++ main.c
@@ -1,143 +1,119 @@
 /*
- * lcd_1.c
+ * RGB_sens.c
  *
- * Created: 04.10.2020 23:49:14
+ * Created: 30.10.2020 19:31:41
  * Author : User
  */ 
+
 
 #define F_CPU 8000000UL
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
-#define lcd_address 0x27
-#define RS 0
-#define E 2
-
+#define RGB_addr 0x29 // RGB sensor's i2c address is 41
+#define repeated 0
+#define Auto_inc 1 
 
 void i2c_start(void){
 	
 	TWCR = (1<<TWINT)|(1<<TWSTA)|(1<<TWEN);
 	while (!(TWCR & (1<<TWINT)));
-	
+
 }
 
-void i2c_data(unsigned char data){
+void i2c_write(unsigned char data){
+	
 	TWDR = data;
 	TWCR = (1<<TWINT)|(1<<TWEN);
 	while (!(TWCR & (1<<TWINT)));
+	
 }
+
+unsigned char i2c_read(void){
+
+	TWCR = (1<<TWINT)|(1<<TWEN);
+	while (!(TWCR & (1<<TWINT)));				
+	return(TWDR);
+	
+}
+
 
 
 void i2c_stop(void){
 	
 	TWCR = (1<<TWINT)|(1<<TWSTO)|(1<<TWEN);
 	while (!(TWCR & (1<<TWINT)));
+
+}
+
+void select_reg(unsigned char reg_addr,unsigned char mode){// mode 00 - Repeated byte, 01 - Auto-increment
+	
+	i2c_write((1<<7)|(mode<<5)|reg_addr);
 	
 }
 
-void LCD_cmd(unsigned char cmd) {
+void RGB_init(void){
+	
+	i2c_start();
+	i2c_write(RGB_addr<<1);// 7 bit RGB sensor's address + W (0)
+	select_reg(0x03,repeated);//Wait Time Register
+	i2c_write(0xFF);// Wait time 2.4 ms
 
-	unsigned char cmd_h=(cmd & 0xF0)|(1<<3);
-	unsigned char cmd_l=((cmd & 0x0F)<<4)|(1<<3);
-
-	cmd_h |= (1<<E);
-	i2c_data(cmd_h);
-	_delay_us(5);
+	i2c_start();
+	i2c_write(RGB_addr<<1);// 7 bit RGB sensor's address + W (0)
+	select_reg(0x01,repeated);//RGBC Timing Register
+	i2c_write(0xFF);//integration time is 2.4 ms
 	
-	cmd_h &= ~(1<<E);
-	i2c_data(cmd_h);
-	_delay_ms(100);
+	i2c_start();
+	i2c_write(RGB_addr<<1);// 7 bit RGB sensor's address + W (0)
+	select_reg(0x00,repeated);//Enable Register
+	i2c_write(0x09);//Power on, Wait enable
 	
-	cmd_l |= (1<<E);
-	i2c_data(cmd_l);
-	_delay_us(5);
+	_delay_ms(3);//delay for RGBC initialization
 	
-	cmd_l &= ~(1<<E);
-	i2c_data(cmd_l);
-	_delay_ms(100);
+	i2c_start();
+	i2c_write(RGB_addr<<1);// 7 bit RGB sensor's address + W (0)
+	select_reg(0x00,repeated);//Enable Register
+	i2c_write(0x0B);//RGBC enable
 	
 }
 
-void LCD_init(void){
+unsigned int read_clear(){
 	
-	LCD_cmd(0x33);
-	LCD_cmd(0x32);
-	LCD_cmd(0x28);//4-bit mode
-	LCD_cmd(0x0D);//Enable display, blinking cursor
-	LCD_cmd(0x06);//right shift of cursor
-	LCD_cmd(0x01);//clear display
-}
-void LCD_data(unsigned char lcd_data){
-	
-	unsigned char lcd_data_h=(lcd_data & 0xF0)|(1<<RS)|(1<<3);
-	unsigned char lcd_data_l=(((lcd_data & 0x0F)<<4))|(1<<RS)|(1<<3);
-
-	lcd_data_h |= (1<<E);
-	_delay_us(5);
-	i2c_data(lcd_data_h);
+	i2c_start();
+	i2c_write(RGB_addr<<1);// 7 bit RGB sensor's address + W (0)
+	select_reg(0x14,repeated);//CDATAL byte
+	i2c_start();
+	i2c_write((RGB_addr<<1)|(1<<0));// 7 bit RGB sensor's address + R (1)
+	unsigned int clear_data_l=i2c_read();
 	
 
-	lcd_data_h &= ~(1<<E);
-	_delay_ms(100);
-	i2c_data(lcd_data_h);
 	
-	lcd_data_l |= (1<<E);_delay_ms(100);
-	_delay_us(5);
-	i2c_data(lcd_data_l);
-	
-	lcd_data_l &= ~(1<<E);
-	_delay_ms(100);
-	i2c_data(lcd_data_l);
-	
-}
+	i2c_start();
+	i2c_write(RGB_addr<<1);// 7 bit RGB sensor's address + W (0)
+	select_reg(0x15,repeated);//CDATAH byte
+	i2c_start();
+	i2c_write((RGB_addr<<1)|(1<<0));// 7 bit RGB sensor's address + R (1)
+	unsigned int clear_data_h=i2c_read();
 
-void stroka(unsigned char *array){
-	unsigned char i=0;
-	while(array[i]!='\0'){
-		LCD_data(array[i]);
-		i++;
-	}
-}
 
-void init_CGRAM_symb(void){
-	
-	LCD_cmd(0x40);// CGRAM address 0x00
-	LCD_data(0x00);//       
-	LCD_data(0x00);//      
-	LCD_data(0x00);//      
-	LCD_data(0x0A);//       * *
-	LCD_data(0x1F);//      *****
-	LCD_data(0x1F);//      *****
-	LCD_data(0x0E);//       ***
-	LCD_data(0x04);//        *
-	
-	
-	LCD_data(0x1F);//     *****
-	LCD_data(0x11);//     *   *
-	LCD_data(0x11);//     *   *
-	LCD_data(0x15);//     * * *
-	LCD_data(0x15);//     * * *
-	LCD_data(0x11);//     *   *
- 	LCD_data(0x11);//     *   *
- 	LCD_data(0x1F);//     *****
+	return((clear_data_h<<8)|clear_data_l);
 	
 }
 
 int main(void)
 {
-	unsigned char addr=(lcd_address<<1); //7 address bits + W bit (0)
-    TWBR|=(1<<TWBR5);// TWBR=32 (for 100 kHz i2c frequency)
-	i2c_start();
-	i2c_data(addr);
-	LCD_init();
-	stroka("Symbol1:");// string 1 output
-	LCD_data(0x00);// output CGRAM user's symbol 1
-	LCD_cmd((1<<7)|0x40);// place cursor to the second line
-	stroka("Symbol2:");// string 2 output
-	LCD_data(0x01);// output CGRAM user's symbol 2
-    i2c_stop();
-	while (1)
-	{
+	unsigned int red=0,green=0,blue=0,clear=0;
+	DDRC=0x01;
+	TWBR|=(1<<TWBR5);// TWBR=32 (for 100 kHz i2c frequency)
+	RGB_init();
+	clear=read_clear();
+	if(clear<20)
+	PORTC=1;
+	else PORTC=0;
+	i2c_stop();
+
+	while (1){
+		
 	}
 }
-
