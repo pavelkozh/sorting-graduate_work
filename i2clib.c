aaae5eedd3679faef3a61f487628a4364919eab2
pavelkozh <72147163+pavelkozh@users.noreply.github.com>
2021-01-13 15:05:10 +0300

    Libraries with functions for i2c protocol
    This library contains functions i2cStart, i2cWrite, i2cRead and i2cStop for data transmissions.

---
+++ i2clib.c
@@ -0,0 +1,31 @@
+#include "i2clib.h"
+
+
+void i2cStart(void){
+	
+	TWCR = (1<<TWINT)|(1<<TWSTA)|(1<<TWEN);
+	while (!(TWCR & (1<<TWINT)));
+
+}
+
+void i2cWrite(uint8_t data){
+	
+	TWDR = data;
+	TWCR = (1<<TWINT)|(1<<TWEN);
+	while (!(TWCR & (1<<TWINT)));
+	
+}
+
+uint8_t i2cRead(void){
+
+	TWCR = (1<<TWINT)|(1<<TWEN)|(1<<TWEA);
+	while (!(TWCR & (1<<TWINT)));
+	return TWDR;
+	
+}
+
+void i2cStop(void){
+	
+	TWCR = (1<<TWINT)|(1<<TWSTO)|(1<<TWEN);
+
+}
