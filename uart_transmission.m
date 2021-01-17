99d7acc1d756b878f71b730563795380b98dd57a
pavelkozh <72147163+pavelkozh@users.noreply.github.com>
2021-01-17 22:24:51 +0300

    Update uart_transmission.m
    This function allows to take a single measurement by pressing a key 's' and display a code of colour (0 - red; 1 - yellow; 2 - green; 3 - light blue; 4 - blue; 5 - pink). 
    To close UART connection press 'f'.

--- uart_transmission.m
+++ uart_transmission.m
@@ -5,20 +5,32 @@ global dat
 delete(instrfind);
 dat = serial('COM5', 'BaudRate', 9600);
 dat.InputBufferSize = 4096;
-
+set(gcf,'CurrentCharacter','!','WindowStyle','modal');
 fopen(dat); 
 set(dat, 'ByteOrder', 'littleEndian'); 
 disp 'Connection is ready!'
-total=[];
-for i=1:1000
-    red = fread(dat,2,'uint8');
-    red = typecast(uint8([red(1), red(2)]), 'uint16');
-    green = fread(dat,2,'uint8');
-    green = typecast(uint8([green(1), green(2)]), 'uint16');
-    blue = fread(dat,2,'uint8');
-    blue = typecast(uint8([blue(1), blue(2)]), 'uint16');
-    cur=[red green blue];
-    total=[total;cur]
+figure(1)
+pause(10/1000)
+while(1)
+
+    key=get(gcf,'CurrentCharacter');
+    if(key=='s')
+        
+        fwrite(dat, 's', 'uint8'); 
+        colour_defined=fread(dat, 1, 'uint8');%1 - yes;0 - no 
+        if(colour_defined==1)
+            colour_code=fread(dat, 1, 'uint8')
+        elseif (colour_defined==0)
+            disp 'Colour has not defined.'
+        end
+        
+    elseif (key=='f')
+        close all
+        break;
+    end
+    set(gcf,'CurrentCharacter','!');
+  pause(10/1000)
 end
-fclose(dat);%closing connection
+
+fclose(dat);
 disp 'Connection is closed!'
